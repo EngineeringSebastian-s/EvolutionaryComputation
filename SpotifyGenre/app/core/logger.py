c20e051946bbdb841f333dc62bae37efe4379a8e
Sebastián López O <sebastianlopezosorno2005@gmail.com>
2026-05-08 12:35:17 -0500

    Create logger.py

---
+++ SpotifyGenre/app/core/logger.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    # Evitar duplicar logs si ya tiene handlers
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+    return logger
